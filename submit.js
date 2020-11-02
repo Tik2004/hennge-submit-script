3f0b03e36e72cb59e88332c07690f06c8a9d6ad9
Tiko <65791040+Tik2004@users.noreply.github.com>
2020-11-02 21:03:41 +0400

    Create submit.js

---
+++ submit.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+const base64 = require('base-64');
+const utf8 = require('utf8');
+const hotpTotpGenerator = require('hotp-totp-generator');
+
+const ReqJSON = {
+  github_url: 'GITHUB_REPO',
+  contact_email: 'YOUR_MAIL',
+};
+
+const stringData = JSON.stringify(ReqJSON);
+const URL = 'CHALLENGE_URL';
+const sharedSecret = ReqJSON.contact_email + 'SPECIAL_CODE';
+
+const MyTOTP = hotpTotpGenerator.totp({
+  key: sharedSecret,
+  T0: 0,
+  X: 30,
+  algorithm: 'sha512',
+  digits: 10,
+});
+
+const authStringUTF = ReqJSON.contact_email + ':' + MyTOTP;
+const bytes = utf8.encode(authStringUTF);
+const encoded = base64.encode(bytes);
+
+const createReq = async () => {
+  try {
+    const config = {
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+         Authorization: 'Basic ' + encoded,
+      },
+    };
+
+    console.log('Making request', { URL, ReqJSON, config });
+
+    const response = await axios.post(URL, stringData, config);
+    console.log(response.data);
+  } catch (err) {
+    console.error(err.response.data);
+  }
+};
+
+createReq();
